b601b3677324af9c3106fae4fa5cdd67de71dd65
Prima20 <primakurnia71@gmail.com>
2018-10-31 13:49:55 +0700

    Add check code layout

---
+++ app/src/main/java/com/rpl/kelompok7/sportification/CheckCodeActivity.java
@@ -0,0 +1,14 @@
+package com.rpl.kelompok7.sportification;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class CheckCodeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_code);
+
+    }
+}
